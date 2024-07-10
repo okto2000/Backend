83fb2d48e6fd8ca02d8bf261efa72b585e40e41c
okto2000 <oktorino2000@gmail.com>
2024-07-10 21:55:12 +0700

    request

---
+++ app/Http/Requests/NewEmployeeRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class NewEmployeeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+            'notelp' => 'required|string|max:255',
+            'salary' => 'required|numeric|not_in:0',
+            'status' => 'required',
+            'role' => 'required',
+            'email' => 'required|string|email|max:255|unique:employees',
+            'password' => 'required|string|min:8',
+        ];
+    }
+}
